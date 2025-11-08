c5d91f4c2190a37aca30896c94ef0b80852ae967
Yaong <justapoland@gmail.com>
2025-11-08 17:51:34 +0100

    Create scenes.py
    Module file that returns the scenes list

---
+++ python/scenes.py
@@ -0,0 +1,38 @@
+# Scenes.py
+# Module file that has the task to return the list of the scenes thanks to the function get_scene_list
+# TODO: Use a text file or a JSON file to store the scenes list in a better way
+
+
+def get_scene_list(beast_name):
+    return [
+    "You walk into a forest and find a " + beast_name + "!",
+    "You go into the long grass and find a " + beast_name + "!",
+    "You find a castle. At the door, you are greeted with a " + beast_name + "!",
+    "You are walking through a castle when you found a " + beast_name + "!",
+    "A wild " + beast_name + " appeared!",
+    "You wandered into a cave and a " + beast_name + " appears from around the corner!",
+    "While wandering around a mansion you encounter a " + beast_name + "!",
+    "While looking around in a forest, a " + beast_name + " starts running towards you!",
+    "You find a mansion. At the door, you are greeted by a " + beast_name + "!",
+    "While spelunking you are met with a " + beast_name + "!",
+    "While leaving a forest, a " + beast_name + " runs up from behind you!",
+    "While checking out a cave, you are met with a " + beast_name + "!",
+    "While scouting out an area, you see a " + beast_name + " running towards you!",
+    "You are looking around a dungeon when a " + beast_name + " approaches you!",
+    "You were looking around an abandoned town when you spotted a " + beast_name + " running towards you!",
+    "You were exploring abandoned buildings when you found a " + beast_name + "!",
+    "You were exploring a cave with masks on the wall, and you spotted a " + beast_name + "!",
+    "You killed a beast and out of its corpse comes another " + beast_name + "!",
+    "While exploring the Chernobyl exclusion zone, you encounter a " + beast_name + "!",
+    "You look into the door of an abandoned building, and you are met with a " + beast_name + "!",
+    "You are walking through a cemetery and you encounter a " + beast_name + "!",
+    "You are wandering around the courtyard of a mansion, and you encounter a " + beast_name + "!",
+    "You stumbled upon a building and a " + beast_name + " appeared!",
+    "You investigate the cupboard of an abandoned building, and you find a " + beast_name + " inside!",
+    "You are riding around on your mobility scooter, and you find a " + beast_name + "!",
+    "You found a bunch of peach trees, but then you saw a " + beast_name + " running towards you!",
+    "You fell into a pit and there was a " + beast_name + "!",
+    "You were questioning your life choices when a " + beast_name + " spawned in!",
+    "While you were eating microplastics, a " + beast_name + " appeared!",
+    "While out drinking copious amounts of alcohol you ran into a " + beast_name + "!",
+    "I don't know, here's a " + beast_name + "."]
